0fbc308158abfd048355cc54cdef80fb9965d627
dariusw22 <dariusw22.dw@gmail.com>
2018-03-06 18:01:01 -0500

    adding up to date files for the new functions added.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,29 +10,57 @@ var connection = mysql.createConnection({
 });
 
 //The app should then prompt users with two messages.
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
-
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    afterConnection();
+    // run the start function after the connection is made to prompt the user
+    start();
 });
 
-function afterConnection() {
+function start() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         console.log(res);
         connection.end();
-    });
-};.then.inquirer.prompt({
-    name: "action",
-    type: "list",
-    message: "What would you like to do?",
-    choices: [
-        "1. What's the ID of the product you would like to buy?",
-        "2. How many units would you like to buy?"
-    ]
+
+        inquirer.prompt({
+            name: "options",
+            type: "rawlist",
+            message: "Choose the item that you would like to buy.",
+            choices: [
+                "1. What's the ID of the product you would like to buy?",
+                "2. How many units would you like to buy?"
+            ]
+        }).then(function (answer) {
+            // based on their answer, either call the bid or the post functions
+            if (answer.postOrBid.toUpperCase() === "POST") {
+                postAuction();
+            }
+            else {
+                bidAuction();
+            }
+        });
+
 
 // Display all of the items available for sale. Include the ids, names, and prices of products for sale.
+
+const itemsForSale = SELECT * FROM products;
+
+function display(params) {
+    // log all of the products for sale
+    console.log(itemsForSale);
+}
+
 // Once the user has placed the order, the app should check to see if it has enough in stock.
+const inStock =  process.argv()// number of products for sale for, the specific item determined by the unit id.
+function stock() {
+    if (if inStock = inStock) {
+        // subtract number of units purchased.
+        // if item is not inStock prompt the user to select another option.
+        prompt ({
+            name: "check",
+            type: "input",
+            message: "Not enough units please re-select the number of units."
+        })
+    }
+};
+stock();
